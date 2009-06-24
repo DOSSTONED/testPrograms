e97863ab7a861b8a3e35212f11de9757061aa0cd
DOSSTONED <DOSSTONED@vista.aero>
2009-06-24 20:57:04 +0800

    added listbox for logging, add recover hidden directory function, ico to standalone file

--- VB/UDisk/UDisk/Form1.Designer.vb
+++ VB/UDisk/UDisk/Form1.Designer.vb
@@ -29,6 +29,9 @@ Partial Class MainForm
         Me.StatusStrip1 = New System.Windows.Forms.StatusStrip
         Me.ToolStripStatusEnable = New System.Windows.Forms.ToolStripStatusLabel
         Me.CheckBoxClean1 = New System.Windows.Forms.CheckBox
+        Me.Label = New System.Windows.Forms.Label
+        Me.ListBox = New System.Windows.Forms.ListBox
+        Me.Label_Events = New System.Windows.Forms.Label
         CType(Me.TrackBar, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
@@ -45,11 +48,11 @@ Partial Class MainForm
         'TrackBar
         '
         Me.TrackBar.LargeChange = 10
-        Me.TrackBar.Location = New System.Drawing.Point(12, 167)
+        Me.TrackBar.Location = New System.Drawing.Point(12, 196)
         Me.TrackBar.Maximum = 100
-        Me.TrackBar.Minimum = 10
+        Me.TrackBar.Minimum = 20
         Me.TrackBar.Name = "TrackBar"
-        Me.TrackBar.Size = New System.Drawing.Size(268, 45)
+        Me.TrackBar.Size = New System.Drawing.Size(187, 45)
         Me.TrackBar.TabIndex = 1
         Me.TrackBar.TickFrequency = 2
         Me.TrackBar.Value = 100
@@ -89,11 +92,41 @@ Partial Class MainForm
         Me.CheckBoxClean1.Text = "Recover Directories"
         Me.CheckBoxClean1.UseVisualStyleBackColor = True
         '
+        'Label
+        '
+        Me.Label.AutoSize = True
+        Me.Label.Location = New System.Drawing.Point(10, 181)
+        Me.Label.Name = "Label"
+        Me.Label.Size = New System.Drawing.Size(137, 12)
+        Me.Label.TabIndex = 4
+        Me.Label.Text = "Transparent:(20%-100%)"
+        '
+        'ListBox
+        '
+        Me.ListBox.FormattingEnabled = True
+        Me.ListBox.ItemHeight = 12
+        Me.ListBox.Location = New System.Drawing.Point(160, 60)
+        Me.ListBox.Name = "ListBox"
+        Me.ListBox.Size = New System.Drawing.Size(120, 112)
+        Me.ListBox.TabIndex = 5
+        '
+        'Label_Events
+        '
+        Me.Label_Events.AutoSize = True
+        Me.Label_Events.Location = New System.Drawing.Point(158, 45)
+        Me.Label_Events.Name = "Label_Events"
+        Me.Label_Events.Size = New System.Drawing.Size(47, 12)
+        Me.Label_Events.TabIndex = 4
+        Me.Label_Events.Text = "Events:"
+        '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(292, 266)
+        Me.Controls.Add(Me.ListBox)
+        Me.Controls.Add(Me.Label_Events)
+        Me.Controls.Add(Me.Label)
         Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.CheckBoxClean1)
         Me.Controls.Add(Me.CheckBoxEnabled)
@@ -117,5 +150,8 @@ Partial Class MainForm
     Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
     Friend WithEvents ToolStripStatusEnable As System.Windows.Forms.ToolStripStatusLabel
     Friend WithEvents CheckBoxClean1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label As System.Windows.Forms.Label
+    Friend WithEvents ListBox As System.Windows.Forms.ListBox
+    Friend WithEvents Label_Events As System.Windows.Forms.Label
 
 End Class
