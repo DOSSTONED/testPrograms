9973db7d5a8268872127eab8257321ff39f43f7b
DOSSTONED <DOSSTONED@vista.aero>
2009-06-24 20:57:04 +0800

    Remove ico, changed layout, add trackbar

---
+++ VB/UDisk/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.NotifyIconAutoRun = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.Menu_Enable = New System.Windows.Forms.ToolStripMenuItem
+        Me.Menu_Exit = New System.Windows.Forms.ToolStripMenuItem
+        Me.CheckBoxAddFile = New System.Windows.Forms.CheckBox
+        Me.ButtonExit = New System.Windows.Forms.Button
+        Me.ContextMenuStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'NotifyIconAutoRun
+        '
+        Me.NotifyIconAutoRun.ContextMenuStrip = Me.ContextMenuStrip
+        Me.NotifyIconAutoRun.Icon = CType(resources.GetObject("NotifyIconAutoRun.Icon"), System.Drawing.Icon)
+        Me.NotifyIconAutoRun.Text = "DOSSTONED_AutoRun"
+        Me.NotifyIconAutoRun.Visible = True
+        '
+        'ContextMenuStrip
+        '
+        Me.ContextMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Menu_Enable, Me.Menu_Exit})
+        Me.ContextMenuStrip.Name = "ContextMenuStrip"
+        Me.ContextMenuStrip.Size = New System.Drawing.Size(173, 48)
+        '
+        'Menu_Enable
+        '
+        Me.Menu_Enable.CheckOnClick = True
+        Me.Menu_Enable.Name = "Menu_Enable"
+        Me.Menu_Enable.Size = New System.Drawing.Size(172, 22)
+        Me.Menu_Enable.Text = "Enable Protection"
+        '
+        'Menu_Exit
+        '
+        Me.Menu_Exit.Name = "Menu_Exit"
+        Me.Menu_Exit.Size = New System.Drawing.Size(172, 22)
+        Me.Menu_Exit.Text = "Exit(&E)"
+        '
+        'CheckBoxAddFile
+        '
+        Me.CheckBoxAddFile.AutoSize = True
+        Me.CheckBoxAddFile.Location = New System.Drawing.Point(12, 12)
+        Me.CheckBoxAddFile.Name = "CheckBoxAddFile"
+        Me.CheckBoxAddFile.Size = New System.Drawing.Size(204, 16)
+        Me.CheckBoxAddFile.TabIndex = 1
+        Me.CheckBoxAddFile.Text = "添加DOSSTONED认证的AutoRun文件"
+        Me.CheckBoxAddFile.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 266)
+        Me.Controls.Add(Me.CheckBoxAddFile)
+        Me.Controls.Add(Me.ButtonExit)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "MainForm"
+        Me.Text = "AutoRun 保护器"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.ContextMenuStrip.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents NotifyIconAutoRun As System.Windows.Forms.NotifyIcon
+    Friend WithEvents ContextMenuStrip As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents Menu_Enable As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Menu_Exit As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CheckBoxAddFile As System.Windows.Forms.CheckBox
+    Friend WithEvents ButtonExit As System.Windows.Forms.Button
+
+End Class
--- VB/UDisk/UDisk/Form1.Designer.vb
+++ VB/UDisk/UDisk/Form1.Designer.vb
@@ -22,84 +22,100 @@ Partial Class MainForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
-        Me.NotifyIconAutoRun = New System.Windows.Forms.NotifyIcon(Me.components)
-        Me.ContextMenuStrip = New System.Windows.Forms.ContextMenuStrip(Me.components)
-        Me.Menu_Enable = New System.Windows.Forms.ToolStripMenuItem
-        Me.Menu_Exit = New System.Windows.Forms.ToolStripMenuItem
-        Me.CheckBoxAddFile = New System.Windows.Forms.CheckBox
         Me.ButtonExit = New System.Windows.Forms.Button
-        Me.ContextMenuStrip.SuspendLayout()
+        Me.TrackBar = New System.Windows.Forms.TrackBar
+        Me.CheckBoxEnabled = New System.Windows.Forms.CheckBox
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip
+        Me.ToolStripStatusEnable = New System.Windows.Forms.ToolStripStatusLabel
+        Me.CheckBoxClean1 = New System.Windows.Forms.CheckBox
+        CType(Me.TrackBar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'NotifyIconAutoRun
-        '
-        Me.NotifyIconAutoRun.ContextMenuStrip = Me.ContextMenuStrip
-        Me.NotifyIconAutoRun.Icon = CType(resources.GetObject("NotifyIconAutoRun.Icon"), System.Drawing.Icon)
-        Me.NotifyIconAutoRun.Text = "DOSSTONED_AutoRun"
-        Me.NotifyIconAutoRun.Visible = True
-        '
-        'ContextMenuStrip
-        '
-        Me.ContextMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Menu_Enable, Me.Menu_Exit})
-        Me.ContextMenuStrip.Name = "ContextMenuStrip"
-        Me.ContextMenuStrip.Size = New System.Drawing.Size(173, 48)
-        '
-        'Menu_Enable
-        '
-        Me.Menu_Enable.CheckOnClick = True
-        Me.Menu_Enable.Name = "Menu_Enable"
-        Me.Menu_Enable.Size = New System.Drawing.Size(172, 22)
-        Me.Menu_Enable.Text = "Enable Protection"
-        '
-        'Menu_Exit
-        '
-        Me.Menu_Exit.Name = "Menu_Exit"
-        Me.Menu_Exit.Size = New System.Drawing.Size(172, 22)
-        Me.Menu_Exit.Text = "Exit(&E)"
-        '
-        'CheckBoxAddFile
-        '
-        Me.CheckBoxAddFile.AutoSize = True
-        Me.CheckBoxAddFile.Location = New System.Drawing.Point(12, 12)
-        Me.CheckBoxAddFile.Name = "CheckBoxAddFile"
-        Me.CheckBoxAddFile.Size = New System.Drawing.Size(204, 16)
-        Me.CheckBoxAddFile.TabIndex = 1
-        Me.CheckBoxAddFile.Text = "添加DOSSTONED认证的AutoRun文件"
-        Me.CheckBoxAddFile.UseVisualStyleBackColor = True
         '
         'ButtonExit
         '
-        Me.ButtonExit.Location = New System.Drawing.Point(205, 231)
+        Me.ButtonExit.Location = New System.Drawing.Point(205, 218)
         Me.ButtonExit.Name = "ButtonExit"
         Me.ButtonExit.Size = New System.Drawing.Size(75, 23)
-        Me.ButtonExit.TabIndex = 2
+        Me.ButtonExit.TabIndex = 0
         Me.ButtonExit.Text = "退出（&Q）"
         Me.ButtonExit.UseVisualStyleBackColor = True
+        '
+        'TrackBar
+        '
+        Me.TrackBar.LargeChange = 10
+        Me.TrackBar.Location = New System.Drawing.Point(12, 167)
+        Me.TrackBar.Maximum = 100
+        Me.TrackBar.Minimum = 10
+        Me.TrackBar.Name = "TrackBar"
+        Me.TrackBar.Size = New System.Drawing.Size(268, 45)
+        Me.TrackBar.TabIndex = 1
+        Me.TrackBar.TickFrequency = 2
+        Me.TrackBar.Value = 100
+        '
+        'CheckBoxEnabled
+        '
+        Me.CheckBoxEnabled.AutoSize = True
+        Me.CheckBoxEnabled.Location = New System.Drawing.Point(12, 12)
+        Me.CheckBoxEnabled.Name = "CheckBoxEnabled"
+        Me.CheckBoxEnabled.Size = New System.Drawing.Size(126, 16)
+        Me.CheckBoxEnabled.TabIndex = 2
+        Me.CheckBoxEnabled.Text = "Enable Protection"
+        Me.CheckBoxEnabled.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusEnable})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 244)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(292, 22)
+        Me.StatusStrip1.TabIndex = 3
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusEnable
+        '
+        Me.ToolStripStatusEnable.Name = "ToolStripStatusEnable"
+        Me.ToolStripStatusEnable.Size = New System.Drawing.Size(53, 17)
+        Me.ToolStripStatusEnable.Text = "Disabled"
+        '
+        'CheckBoxClean1
+        '
+        Me.CheckBoxClean1.AutoSize = True
+        Me.CheckBoxClean1.Location = New System.Drawing.Point(12, 34)
+        Me.CheckBoxClean1.Name = "CheckBoxClean1"
+        Me.CheckBoxClean1.Size = New System.Drawing.Size(138, 16)
+        Me.CheckBoxClean1.TabIndex = 2
+        Me.CheckBoxClean1.Text = "Recover Directories"
+        Me.CheckBoxClean1.UseVisualStyleBackColor = True
         '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(292, 266)
-        Me.Controls.Add(Me.CheckBoxAddFile)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.CheckBoxClean1)
+        Me.Controls.Add(Me.CheckBoxEnabled)
+        Me.Controls.Add(Me.TrackBar)
         Me.Controls.Add(Me.ButtonExit)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.Name = "MainForm"
-        Me.Text = "AutoRun 保护器"
-        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
-        Me.ContextMenuStrip.ResumeLayout(False)
+        Me.Text = "MainForm"
+        CType(Me.TrackBar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents NotifyIconAutoRun As System.Windows.Forms.NotifyIcon
-    Friend WithEvents ContextMenuStrip As System.Windows.Forms.ContextMenuStrip
-    Friend WithEvents Menu_Enable As System.Windows.Forms.ToolStripMenuItem
-    Friend WithEvents Menu_Exit As System.Windows.Forms.ToolStripMenuItem
-    Friend WithEvents CheckBoxAddFile As System.Windows.Forms.CheckBox
     Friend WithEvents ButtonExit As System.Windows.Forms.Button
+    Friend WithEvents TrackBar As System.Windows.Forms.TrackBar
+    Friend WithEvents CheckBoxEnabled As System.Windows.Forms.CheckBox
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ToolStripStatusEnable As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents CheckBoxClean1 As System.Windows.Forms.CheckBox
 
 End Class
