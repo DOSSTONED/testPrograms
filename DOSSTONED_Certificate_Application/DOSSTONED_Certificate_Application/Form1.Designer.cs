07d10b1a41e0a7e01979f495a476f6921ae3904d
DOSSTONED <dosstoned@vista.aero>
2009-09-05 23:08:52 +0800

    Init commit for project(auto) DOSSTONED_Certificate_Application

---
+++ DOSSTONED_Certificate_Application/DOSSTONED_Certificate_Application/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace DOSSTONED_Certificate_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BtnOK = new System.Windows.Forms.Button();
+            this.NotifyTaskbar = new System.Windows.Forms.NotifyIcon(this.components);
+            this.ContextRhtClk = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TimerDisappear = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.ContextRhtClk.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(85, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Certificated\r\nIt\'s safe!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::DOSSTONED_Certificate_Application.Properties.Resources.DOSSTONED;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 73);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BtnOK
+            // 
+            this.BtnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnOK.Location = new System.Drawing.Point(225, 118);
+            this.BtnOK.Name = "BtnOK";
+            this.BtnOK.Size = new System.Drawing.Size(75, 23);
+            this.BtnOK.TabIndex = 2;
+            this.BtnOK.Text = "OK, I know.";
+            this.BtnOK.UseVisualStyleBackColor = true;
+            this.BtnOK.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // NotifyTaskbar
+            // 
+            this.NotifyTaskbar.ContextMenuStrip = this.ContextRhtClk;
+            this.NotifyTaskbar.Icon = ((System.Drawing.Icon)(resources.GetObject("NotifyTaskbar.Icon")));
+            this.NotifyTaskbar.Text = "notifyIcon1";
+            this.NotifyTaskbar.Visible = true;
+            // 
+            // ContextRhtClk
+            // 
+            this.ContextRhtClk.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.ContextRhtClk.Name = "ContextRhtClk";
+            this.ContextRhtClk.Size = new System.Drawing.Size(104, 48);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // TimerDisappear
+            // 
+            this.TimerDisappear.Interval = 1000;
+            this.TimerDisappear.Tick += new System.EventHandler(this.TimerDisappear_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(300, 141);
+            this.ControlBox = false;
+            this.Controls.Add(this.BtnOK);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ContextRhtClk.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button BtnOK;
+        private System.Windows.Forms.NotifyIcon NotifyTaskbar;
+        private System.Windows.Forms.ContextMenuStrip ContextRhtClk;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.Timer TimerDisappear;
+    }
+}
+
---
+++ DOSSTONED_Certificate_Application/DOSSTONED_Certificate_Application/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace DOSSTONED_Certificate_Application
+{
+    public partial class Form1 : Form
+    {
+        private const int WM_DEVICECHANGE = 0x219;
+        private const int DBT_DEVICEARRIVAL = 0x8000;
+        private const int DBT_CONFIGCHANGECANCELED = 0x19;
+        private const int DBT_CONFIGCHANGED = 0x18;
+        private const int DBT_CUSTOMEVENT = 0x8006;
+        private const int DBT_DEVICEQUERYREMOVE = 0x8001;
+        private const int DBT_DEVICEQUERYREMOVEFAILED = 0x8002;
+        private const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
+        private const int DBT_DEVICEREMOVEPENDING = 0x8003;
+        private const int DBT_DEVICETYPESPECIFIC = 0x8005;
+        private const int DBT_DEVNODES_CHANGED = 0x7;
+        private const int DBT_QUERYCHANGECONFIG = 0x17;
+        private const int DBT_USERDEFINED = 0xFFFF;
+
+        private const int WM_CREATE = 0x0001;
+        private int TimeLeft = 3;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        protected override void WndProc(ref   Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_CREATE:
+                    MessageBox.Show(m.ToString());
+                    Show();
+                    TimeLeft = 5;
+                    TimerDisappear.Enabled = true;
+                    break;
+                case DBT_DEVICEARRIVAL:
+                    MessageBox.Show("FlashDisk Insert");
+                    break;
+
+            }
+
+            base.WndProc(ref   m);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Location = new Point(System.Windows.Forms.SystemInformation.WorkingArea.Width - this.Width, System.Windows.Forms.SystemInformation.WorkingArea.Height - this.Height);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Show();
+            TimeLeft = 5;
+            TimerDisappear.Enabled = true;
+        }
+
+        private void TimerDisappear_Tick(object sender, EventArgs e)
+        {
+            if (TimeLeft < 1)
+                Hide();
+            else
+                TimeLeft--;
+        }
+    }
+}
+
+//http://hi.baidu.com/zzzkkk666/blog/item/580baf1bfbacb4188718bf7c.html Here are the showing forms methods
+/*
+ * 
+ * http://blog.csdn.net/swimmer2000/archive/2008/12/01/3420661.aspx
+ * This discribed what Win32 Application will do after it begins to start
+ * 
+ * 
+*/
