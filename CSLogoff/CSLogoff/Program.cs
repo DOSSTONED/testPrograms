b212a3f0837e35a0afb00502277842084781c6d7
DOSSTONED <dosstoned@vista.aero>
2009-09-24 21:31:55 +0800

    Init commit for project(auto) CSLogoff

---
+++ CSLogoff/CSLogoff/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace CSLogoff
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+           Confirmation con= new Confirmation();
+            SystemEvents.SessionSwitch+=new SessionSwitchEventHandler(con.SystemEvents_SessionSwitch);
+            Application.Run(con);
+        }
+    }
+}
