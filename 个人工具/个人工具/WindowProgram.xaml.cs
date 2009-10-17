94b13875cff01a454abf4edd13b5880aecf33b6a
DOSSTONED <dosstoned@vista.aero>
2009-10-17 21:38:44 +0800

    Init commit for project 个人工具

---
+++ 个人工具/个人工具/WindowProgram.xaml.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace 个人工具
+{
+    /// <summary>
+    /// WindowProgram.xaml 的交互逻辑
+    /// </summary>
+    public partial class WindowProgram : Window
+    {
+        private double _top = 10, _left = 10, BorderMargin = 0;
+        
+        /// <summary>
+        /// 
+        /// </summary>
+
+        public WindowProgram()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAddProg_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (File.Exists(textBoxInputPath.Text))
+            {
+                if (textBoxInputName.Text != "")
+                {
+                    Progs p = new Progs(textBoxInputPath.Text, textBoxInputName.Text);
+                    p.Width = 89;
+                    p.Height = 33;
+                    p.Background = Brushes.Transparent;
+                    p.Content = textBoxInputName.Text;
+                    string path = Directory.GetCurrentDirectory() + "\\Data\\FavorProg\\" + textBoxInputName.Text + ".DOSSTONED";// +p.ClickedTimes.ToString();
+
+                    listView1.Items.Add(p);
+
+                    if (!File.Exists(path))
+                    {
+                        using (StreamWriter sw = File.CreateText(path))
+                        {
+                            sw.WriteLine(textBoxInputPath.Text);
+                        }
+                    }
+
+                    textBoxInputPath.Text = "";
+                    textBoxInputName.Text = "";
+                }
+                else
+                    MessageBox.Show("缺少名称！");
+            }
+            else
+            {
+                if (textBoxInputName.Text != "" && textBoxInputPath.Text != "")
+                    MessageBox.Show(new FileNotFoundException().Message);
+            }
+            th.Background = Brushes.Transparent;
+            stackPanel.Visibility = Visibility.Collapsed;
+            th.Visibility = Visibility.Collapsed;
+        }
+
+
+
+        private void buttonBack_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+            
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            th.Visibility = Visibility.Visible;
+            stackPanel.Visibility = Visibility.Visible;
+        }
+
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            const int BorderPixel = 0;
+            this.Left = SystemParameters.WorkArea.Width - Width - BorderPixel;
+            this.Top = SystemParameters.WorkArea.Height - Height - BorderPixel;
+            string[] Programs = Directory.GetFiles(Directory.GetCurrentDirectory() + "\\Data\\FavorProg", "*.DOSSTONED.*");
+            foreach (string program in Programs)
+            {
+                string sname = "";
+                //uint sclick = 0;
+                using (StreamReader sr = File.OpenText(program))
+                {
+                    sname = sr.ReadLine();
+                    //sclick = Convert.ToUInt32(Path.GetExtension(program));
+                }
+                Progs p = new Progs(sname, Path.GetFileNameWithoutExtension(program));
+                p.Width = 89;
+                p.Height = 33;
+                p.Background = Brushes.Transparent;
+                //p.ClickedTimes = sclick;
+                listView1.Items.Add(p);
+            }
+            
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //string[] Programs = Directory.GetFiles(Directory.GetCurrentDirectory() + "\\Data\\FavorProg", "*.DOSSTONED");
+            //foreach (string program in Programs)
+            //{
+            //    string sname = "";
+            //    uint sclick = 0;
+            //    using (StreamReader sr = File.OpenText(program))
+            //    {
+            //        sname = sr.ReadLine();
+            //        sclick = Convert.ToUInt32(sr.ReadLine());
+            //    }
+            //}
+        }
+
+
+        [DllImport("user32.dll")]
+        static extern IntPtr FindWindow(string className, string windowName);
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hwndChild, IntPtr hwndNewParent);
+
+        private void buttonChangeParent_Click(object sender, RoutedEventArgs e)
+        {
+            IntPtr desktop = FindWindow(null, "Program Manager");
+            IntPtr hwndOldParent = SetParent(new WindowInteropHelper(this).Handle, desktop);
+        }
+
+
+
+
+        private void DragThumb_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+            _left = Left;
+            _top = Top;
+            _left += (int)e.HorizontalChange;
+            _top += (int)e.VerticalChange;
+
+            if (_top < BorderMargin)
+            {
+                _top = BorderMargin;
+            }
+            if (_left < BorderMargin)
+            {
+                _left = BorderMargin;
+            }
+            if (_top > SystemParameters.WorkArea.Height - Height - BorderMargin)
+            {
+                _top = SystemParameters.WorkArea.Height - Height - BorderMargin;
+            }
+            if (_left > SystemParameters.WorkArea.Width - Width - BorderMargin)
+            {
+                _left = SystemParameters.WorkArea.Width - Width - BorderMargin;
+            }
+            this.Left = _left;
+            this.Top = _top;
+        }
+
+        
+
+
+    }
+}
---
+++ 个人工具/个人工具/类库/AeroGlass.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+
+namespace 个人工具
+{
+    class AeroGlass
+    {
+        struct MARGINS
+        {
+            public MARGINS(Thickness t)
+            {
+                Left = (int)t.Left;
+                Right = (int)t.Right;
+                Top = (int)t.Top;
+                Bottom = (int)t.Bottom;
+            }
+            public int Left;
+            public int Right;
+            public int Top;
+            public int Bottom;
+
+        }
+
+        [DllImport("dwmapi.dll", PreserveSig = false)]
+        static extern void DwmExtendFrameIntoClientArea(IntPtr hwnd, ref MARGINS margins);
+
+        [DllImport("dwmapi.dll", PreserveSig = false)]
+        static extern bool DwmIsCompositionEnabled();
+
+
+        public static bool ExtendGlassFrame(Window window, Thickness margin)
+        {
+            IntPtr hwnd = new WindowInteropHelper(window).Handle;
+            if (hwnd == IntPtr.Zero)
+                throw new InvalidOperationException("窗口由于未被绘制而无法使用玻璃特效.");
+
+            if (System.Environment.OSVersion.Version.Major >= 6)
+            {
+                if (DwmIsCompositionEnabled())
+                {
+                    window.Background = Brushes.Transparent;
+                    HwndSource.FromHwnd(hwnd).CompositionTarget.BackgroundColor = Colors.Transparent;
+
+                    MARGINS margins = new MARGINS(margin);
+                    DwmExtendFrameIntoClientArea(hwnd, ref margins);
+                }
+                else
+                {
+                    MessageBox.Show("玻璃特效未打开！");
+                }
+            }
+            else
+            {
+                MessageBox.Show("系统不是 Windows Vista 或者 Windows 7.");
+            }
+
+
+            //本文来自CSDN博客，转载请标明出处：http://blog.csdn.net/SnowRen3074/archive/2008/10/23/3129306.aspx
+            // Set the background to transparent from both the WPF and Win32 perspectives
+
+            //SolidColorBrush background = new SolidColorBrush(Colors.Green);
+            //background.Opacity = 0.5;
+
+
+            return true;
+        }
+    }
+}
