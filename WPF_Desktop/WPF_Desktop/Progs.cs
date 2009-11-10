f010ec9e2656039e2364d41e69316d8623c72589
DOSSTONED <dosstoned@vista.aero>
2009-11-10 20:53:52 +0800

    Init commit for project(auto) WPF_Desktop

---
+++ WPF_Desktop/WPF_Desktop/Progs.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Media;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using System.Windows.Interop;
+using System.Windows;
+
+public class ExtractIcon
+{
+    [DllImport("Shell32.dll")]
+    private static extern int SHGetFileInfo(
+        string pszPath,
+        uint dwFileAttributes,
+        out     SHFILEINFO psfi,
+        uint cbfileInfo,
+        SHGFI uFlags
+        );
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct SHFILEINFO
+    {
+        public SHFILEINFO(bool b)
+        {
+            hIcon = IntPtr.Zero;
+            iIcon = 0;
+            dwAttributes = 0;
+            szDisplayName = "";
+            szTypeName = "";
+        }
+        public IntPtr hIcon;
+        public int iIcon;
+        public uint dwAttributes;
+        [MarshalAs(UnmanagedType.LPStr, SizeConst = 260)]
+        public string szDisplayName;
+        [MarshalAs(UnmanagedType.LPStr, SizeConst = 80)]
+        public string szTypeName;
+    };
+
+    private ExtractIcon()
+    {
+    }
+
+    private enum SHGFI
+    {
+        SmallIcon = 0x00000001,
+        LargeIcon = 0x00000000,
+        Icon = 0x00000100,
+        DisplayName = 0x00000200,
+        Typename = 0x00000400,
+        SysIconIndex = 0x00004000,
+        UseFileAttributes = 0x00000010
+    }
+
+    public static Icon GetIcon(string strPath, bool bSmall)
+    {
+        SHFILEINFO info = new SHFILEINFO(true);
+        int cbFileInfo = Marshal.SizeOf(info);
+        SHGFI flags;
+        if (bSmall)
+        {
+            flags = SHGFI.Icon | SHGFI.SmallIcon | SHGFI.UseFileAttributes;
+        }
+        else
+        {
+            flags = SHGFI.Icon | SHGFI.LargeIcon | SHGFI.UseFileAttributes;
+        }
+        SHGetFileInfo(strPath, 256, out   info, (uint)cbFileInfo, flags);
+        return Icon.FromHandle(info.hIcon);
+    }
+
+}
+
+
+
+//[Serializable]
+class Progs : System.Windows.Controls.Image
+{
+    public Progs(string path, string name)
+    {
+
+        //Name
+        //Content = Image.FromFile(path);
+
+        // Image i = 
+        IntPtr hBitmap = ExtractIcon.GetIcon(path, false).ToBitmap().GetHbitmap();
+
+        ImageSource wpfBitmap =
+            Imaging.CreateBitmapSourceFromHBitmap(
+                 hBitmap, IntPtr.Zero, Int32Rect.Empty,
+                 BitmapSizeOptions.FromEmptyOptions());
+        // i.Width = 48;
+        // i.Height = 48;
+
+        // BitmapImage bi = new BitmapImage();
+        // bi.BeginInit();
+        // bi = ExtractIcon.GetIcon(path, false);
+        // bi.UriSource = new Uri("pack://application:,,,/Resources/close_small_black.ico");
+        // bi.EndInit();
+        // i.Source = bi;
+
+        this.Source = wpfBitmap;
+
+        //Content = ExtractIcon.GetIcon(path, false).ToBitmap();
+        ImagePath = path;
+        
+        Width = 72;
+        Height = 64;
+        // Background = System.Windows.Media.Brushes.Transparent;
+        ToolTip = name;
+        Margin = new System.Windows.Thickness(10);
+        Opacity = 0.8;
+        this.MouseLeftButtonUp += new System.Windows.Input.MouseButtonEventHandler(Progs_ClickThread);
+        this.MouseEnter += new System.Windows.Input.MouseEventHandler(Progs_MouseEnter);
+        this.MouseLeave += new System.Windows.Input.MouseEventHandler(Progs_MouseLeave);
+    }
+
+    void Progs_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+        Opacity = 0.8;
+    }
+
+    void Progs_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+    {
+        Opacity = 1;
+    }
+
+    void Progs_ClickThread(object sender, System.Windows.RoutedEventArgs e)
+    {
+        Thread t = new Thread(new ThreadStart(Progs_Click));
+        t.Start();
+    }
+
+    void Progs_Click()
+    {
+        try
+        {
+
+            System.Diagnostics.Process.Start(ImagePath);
+            ClickedTimes++;
+        }
+        catch (Exception ex)
+        {
+            System.Windows.MessageBox.Show(ex.Message);
+        }
+    }
+
+    private string _path;
+    private string _name;
+    private uint _clicktimes;
+
+
+    public uint ClickedTimes
+    {
+        get
+        {
+            return _clicktimes;
+        }
+        set
+        {
+            _clicktimes = value;
+        }
+    }
+
+    public string ImagePath
+    {
+        get
+        {
+            return _path;
+        }
+        set
+        {
+            _path = value;
+        }
+    }
+
+    public string DisplayName
+    {
+        get
+        {
+            return _name;
+        }
+        set
+        {
+            _name = value;
+        }
+    }
+
+}
---
+++ WPF_Desktop/WPF_Desktop/Window1.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Drawing;
+using System.Windows.Interop;
+
+namespace WPF_Desktop
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ButtonAddProg_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (File.Exists(textBoxInputPath.Text))
+            {
+                if (textBoxInputName.Text != "")
+                {
+                    Progs p = new Progs(textBoxInputPath.Text, textBoxInputName.Text);
+                    
+                    string path = Directory.GetCurrentDirectory() + "\\..\\Data\\FavorProg\\" + textBoxInputName.Text + ".DOSSTONED";// +p.ClickedTimes.ToString();
+
+                    listViewProgram.Children.Add(p);
+
+                    if (!File.Exists(path))
+                    {
+                        using (StreamWriter sw = File.CreateText(path))
+                        {
+                            sw.WriteLine(textBoxInputPath.Text);
+                        }
+                    }
+
+                    textBoxInputPath.Text = "";
+                    textBoxInputName.Text = "";
+                }
+                else
+                    MessageBox.Show("缺少名称！");
+            }
+            else
+            {
+                if (textBoxInputName.Text != "" && textBoxInputPath.Text != "")
+                    MessageBox.Show(new FileNotFoundException().Message);
+            }
+            th.Background = System.Windows.Media.Brushes.Transparent;
+            stackPanel.Visibility = Visibility.Collapsed;
+            th.Visibility = Visibility.Collapsed;
+        }
+
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            th.Visibility = Visibility.Visible;
+            stackPanel.Visibility = Visibility.Visible;
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Left = 0;
+            this.Top = 0;
+            this.Width = SystemParameters.WorkArea.Width;
+            this.Height = SystemParameters.WorkArea.Height;
+            string[] Programs = Directory.GetFiles(Directory.GetCurrentDirectory() + "\\..\\Data\\FavorProg", "*.DOSSTONED.*");
+            foreach (string program in Programs)
+            {
+                string sname = "";
+                //uint sclick = 0;
+                using (StreamReader sr = File.OpenText(program))
+                {
+                    sname = sr.ReadLine();
+                    //sclick = Convert.ToUInt32(Path.GetExtension(program));
+                }
+                Progs p = new Progs(sname, System.IO.Path.GetFileNameWithoutExtension(program));
+                //Label a = new Label();
+                //a.Content = "111";
+                //a.Foreground = System.Windows.Media.Brushes.AliceBlue;
+                //p.Content = a;//
+                //System.Drawing.Image icon = System.Drawing.Image.FromFile(@"E:\石道辰\Icons\ICO_6.ico");
+                
+                //p.Content = icon;
+                //p.Width = 1000;
+
+                //p.ClickedTimes = sclick;
+                listViewProgram.Children.Add(p);
+            }
+
+        }
+
+        private void buttonExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+
+        private void QuickStartAdd(Progs p)
+        {
+
+        }
+
+
+        //[DllImport("user32.dll")]
+        //static extern IntPtr FindWindow(string className, string windowName);
+        //[DllImport("user32.dll")]
+        //static extern IntPtr SetParent(IntPtr hwndChild, IntPtr hwndNewParent);
+
+        //private void buttonChangeParent_Click(object sender, RoutedEventArgs e)
+        //{
+        //    IntPtr desktop = FindWindow(null, "Program Manager");
+        //    IntPtr hwndOldParent = SetParent(new WindowInteropHelper(this).Handle, desktop);
+        //}
+
+
+        #region FlashDisk Protect
+
+        #endregion
+
+
+
+
+    }
+}
