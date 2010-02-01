79e1e55fd85c793dddc8bb01ff9e9f52c01df316
DOSSTONED <dosstoned@vista.aero>
2010-02-01 16:17:01 +0800

    Init commit for project(auto) GoogleMusicBox

---
+++ GoogleCodeJam Qualification Round 2012/Problem B. Dancing With the Googlers/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Problem_B.Dancing_With_the_Googlers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+                return;
+            string[] allText = File.ReadAllLines(args[0]);
+            int TotalCases = int.Parse(allText[0]);
+            string outPath = "Problem_B.out";
+            File.WriteAllText(outPath, string.Empty);
+            for (int i = 1; i <= TotalCases; i++)
+            {
+                string[] strs = allText[i].Split(' ');
+                int Number = int.Parse(strs[0]);
+                int Stars = int.Parse(strs[1]);
+                int p = int.Parse(strs[2]);
+                int total = 0;
+                int canUseStar = 0;
+                for (int j = 0; j < Number; j++)
+                {
+                    int CurrentPerson = int.Parse(strs[j + 3]);
+                    if( (CurrentPerson < 29 )&&( CurrentPerson > 1))//28=8+10+10,can use star
+                        canUseStar++;
+                    if (CurrentPerson >= 3 * p - 2)
+                    {
+                        total++;
+
+                        continue;
+                    }
+                    if (CurrentPerson < 3 * p - 4) continue;
+
+                    if (3 * p - 4 > 0)
+                    {
+                        if (Stars > 0)
+                        {
+                            total++; Stars--; canUseStar--;// must use star to gain more totals
+                        }
+                    }
+                    else// 3p-4<0, that is, p=0 or p=1
+                    // this time, CurrentPerson < 3 * p - 2
+                    // which means currentPerson==0 or 1
+                    {
+                        if (CurrentPerson == 0)
+                            if (p == 0)
+                            { total++; continue; }
+                            else continue;
+                        total++;//CurrentPerson==1, p==0or1 both are ok
+                    }
+
+                }
+                if (Stars != 0 && canUseStar < Stars)
+                {
+                    Console.WriteLine(string.Format("Case #{0}: Stars are not all used.", i));
+                    File.AppendAllText(outPath, string.Format("Case #{0}: 0\r\n", i));
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("Case #{0}: {1}", i, total));
+                    File.AppendAllText(outPath, string.Format("Case #{0}: {1}\r\n", i, total));
+                }
+            }
+        }
+    }
+}
---
+++ GoogleCodeJam Qualification Round 2012/Problem C. Recycled Numbers/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Problem_C.Recycled_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+                return;
+            string[] allText = File.ReadAllLines(args[0]);
+            int allCases = int.Parse(allText[0]);
+            string outPath = "Problem_C.out";
+            File.WriteAllText(outPath, string.Empty);
+            for (int i = 1; i < allText.Length; i++)
+            {
+                string[] Numbers = allText[i].Split(' ');
+                if (Numbers.Length == 2)
+                {
+                    int A = int.Parse(Numbers[0]);
+                    int B = int.Parse(Numbers[1]);
+                    int totalCase = 0;
+                    for (int j = A; j <= B; j++)
+                    {
+                        int[] RecycledJ = GiveRecycleNumbers(j);
+                        if (RecycledJ == null)
+                        {
+                            break;
+                        }
+                        for (int k = 0; k < RecycledJ.Length; k++)
+                        {
+                            if (RecycledJ[k] <= j) continue;
+                            if (RecycledJ[k] > B) continue;
+                            bool isRepeated = false;
+                            for (int kk = k + 1; kk < RecycledJ.Length; kk++)
+                            {
+                                if (RecycledJ[kk] == RecycledJ[k]) isRepeated = true;
+                            }
+                            if (!isRepeated)
+                                totalCase++;// this is not accurate since 1212-> 2121, 2121 two times.
+                            //File.AppendAllText("OutDebug.txt", string.Format("A={0},B={1}, pair:({2},{3})\r\n", A, B, j, rj));
+                        }
+                    }
+                    Console.WriteLine("Case #{0}: {1}\r\n", i, totalCase);
+                    File.AppendAllText(outPath, string.Format("Case #{0}: {1}\r\n", i, totalCase));
+                    
+                }
+
+            }
+        }
+
+        static int[] GiveRecycleNumbers(int num)
+        {
+            int totalDigits = num.ToString().Length;
+            if (totalDigits == 1) return null;
+
+            int[] ret = new int[totalDigits - 1];
+            for (int i = 0; i < totalDigits - 1; i++)
+            {
+
+                ret[i] = (num * (int)Math.Pow(10, i + 1)) % (int)Math.Pow(10, totalDigits) +
+                    (num * (int)Math.Pow(10, i + 1) - ((num * (int)Math.Pow(10, i + 1)) % (int)Math.Pow(10, totalDigits))) / (int)Math.Pow(10, totalDigits)
+                    ;
+            }
+            return ret;
+        }
+    }
+}
