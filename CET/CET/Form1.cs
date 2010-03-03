41eb5536c26dec1d6d5585d6290840ef9a5a6bd3
DOSSTONED <DOSSTONED@vista.aero>
2010-03-03 11:28:50 +0800

    init project CET

---
+++ CET/CET/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace CET
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            HtmlElementCollection htec = webBrowser1.Document.All;
+            for (int i = 0; i < htec.Count; i++)
+            {
+                listBox1.Items.Add(htec[i].InnerHtml);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CookieContainer cookiecontain=new CookieContainer();
+            string str = SendDataByPost("http://cet.99sushe.com/getscore.html", "id=REPLACE_YOUR_ID&vc=novcversion", ref cookiecontain);
+            MessageBox.Show(str);
+        }
+
+        #region 同步通过POST方式发送数据
+        /// <summary>
+        /// 通过POST方式发送数据
+        /// </summary>
+        /// <param name="Url">url</param>
+        /// <param name="postDataStr">Post数据</param>
+        /// <param name="cookie">Cookie容器</param>
+        /// <returns></returns>
+        public string SendDataByPost(string Url, string postDataStr, ref CookieContainer cookie)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+            if (cookie.Count == 0)
+            {
+                request.CookieContainer = new CookieContainer();
+                cookie = request.CookieContainer;
+            }
+            else
+            {
+                request.CookieContainer = cookie;
+            }
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = postDataStr.Length;
+            Stream myRequestStream = request.GetRequestStream();
+            StreamWriter myStreamWriter = new StreamWriter(myRequestStream, Encoding.GetEncoding("gb2312"));
+            myStreamWriter.Write(postDataStr);
+            myStreamWriter.Close();
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            return retString;
+        }
+        #endregion
+
+        #region 同步通过GET方式发送数据
+        /// <summary>
+        /// 通过GET方式发送数据
+        /// </summary>
+        /// <param name="Url">url</param>
+        /// <param name="postDataStr">GET数据</param>
+        /// <param name="cookie">GET容器</param>
+        /// <returns></returns>
+        public string SendDataByGET(string Url, string postDataStr, ref CookieContainer cookie)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url + (postDataStr == "" ? "" : "?") + postDataStr);
+            if (cookie.Count == 0)
+            {
+                request.CookieContainer = new CookieContainer();
+                cookie = request.CookieContainer;
+            }
+            else
+            {
+                request.CookieContainer = cookie;
+            }
+
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            return retString;
+        }
+        #endregion
+    }
+}
+
+   
