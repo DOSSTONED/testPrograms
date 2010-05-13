500684b50c178a90739f4a495c67833915ffb5c6
DC-Shi <daochen.shi@gmail.com>
2010-05-13 22:03:47 +0800

    Init commit for project(auto) 恢复文件夹

---
+++ 恢复文件夹/恢复文件夹/ClassWin6Privileges.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+class ClassWin6Privileges
+{
+    static internal bool IsAdmin()
+    {
+        WindowsIdentity id = WindowsIdentity.GetCurrent();
+        WindowsPrincipal p = new WindowsPrincipal(id);
+        return p.IsInRole(WindowsBuiltInRole.Administrator);
+    }
+
+    public static class VistaSecurity
+    {
+        [DllImport("user32")]
+        public static extern UInt32 SendMessage(IntPtr hWnd, UInt32 msg, UInt32 wParam, UInt32 lParam);
+
+        internal const int BCM_FIRST = 0x1600;
+        internal const int BCM_SETSHIELD = (BCM_FIRST + 0x000C);
+
+        /**/
+        /// <summary>
+        /// Add a shield icon to a button
+        /// </summary>
+        /// <param name="b">The button</param>
+        static internal void AddShieldToButton(Button b)
+        {
+            b.FlatStyle = FlatStyle.System;
+            SendMessage(b.Handle, BCM_SETSHIELD, 0, 0xFFFFFFFF);
+        }
+    }
+
+    internal static void RestartElevated()
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+        startInfo.UseShellExecute = true;
+        startInfo.WorkingDirectory = Environment.CurrentDirectory;
+        startInfo.FileName = Application.ExecutablePath;
+        startInfo.Verb = "runas";
+        try
+        {
+            Process p = Process.Start(startInfo);
+        }
+        catch (System.ComponentModel.Win32Exception ex)
+        {
+            return; //If cancelled, do nothing
+        }
+        Application.Exit();
+    }
+
+}
