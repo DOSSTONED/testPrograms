ed9b77edabb8564172dfaa707a7d8b3ca64254c3
DC-Shi <daochen.shi@gmail.com>
2011-03-13 22:48:53 +0800

    Init commit for project(auto) Give_Words

---
+++ Give_Words/Give_Words/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Give_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessWords.WriteIt(@"E:\SRT\IN");
+        }
+    }
+}
