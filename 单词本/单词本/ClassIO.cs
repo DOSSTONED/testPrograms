c1875aa1ad122dd1e0b8a2bc434fba9d57273d40
DOSSTONED <dosstoned@vista.aero>
2011-03-20 20:02:20 +0800

    Init commit for project(auto) 单词本

---
+++ 单词本/单词本/ClassIO.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Trinet.Core.IO.Ntfs;
+
+namespace 单词本
+{
+    class Word
+    {
+        private string _word;
+        private string _meaning;
+        private double _familiarity;
+        private long _reviewTimes;
+
+        public Word(string word, string meaning, double familiarity,long reviewedTimes)
+        {
+            _word = word;
+            _meaning = meaning;
+            _familiarity = familiarity;
+            _reviewTimes = reviewedTimes;
+
+        }
+
+        public Word(string word, string meaning, double familiarity)
+        {
+            _word = word;
+            _meaning = meaning;
+            _familiarity = familiarity;
+            _reviewTimes = 1;
+        }
+
+        public Word(string word, string meaning)
+        {
+            _word = word;
+            _meaning = meaning;
+            _familiarity = 0;
+            _reviewTimes = 1;
+        }
+
+        public Word(string word)
+        {
+            _word = word;
+            _meaning = string.Empty;
+            _familiarity = 0;
+            _reviewTimes = 1;
+        }
+
+        public string WordName
+        {
+            get
+            {
+                return _word;
+            }
+            set
+            {
+                _word = value;
+            }
+        }
+
+        public string Meaning
+        {
+            get
+            {
+                return _meaning;
+            }
+            set
+            {
+                _meaning = value;
+            }
+        }
+
+        public double Familiarity
+        {
+            get
+            {
+                return _familiarity;
+            }
+            set
+            {
+                _familiarity = value;
+            }
+        }
+
+        public long ReviewedTimes
+        {
+            get
+            {
+                return _reviewTimes;//throw new System.NotImplementedException();
+            }
+            set
+            {
+                _reviewTimes = value;
+            }
+        }
+    }
+    static class ClassIO
+    {
+        static public List<Word> words = null;
+
+        static public void LoadWords()
+        {
+            words = new List<Word>();
+            string[] wordFiles = Directory.GetFiles(WorkingDir);
+            foreach (string wordSingle in wordFiles)
+            {
+                string curMeaning = File.ReadAllText(wordSingle);
+                string curName = Path.GetFileNameWithoutExtension(wordSingle);
+                double famili = 0;
+                long review = 1;
+                FileInfo fInfo = new FileInfo(wordSingle);
+                if (fInfo.AlternateDataStreamExists(StreamNameFamili))
+                {
+                    StreamReader sr = fInfo.GetAlternateDataStream(StreamNameFamili).OpenText();
+                    famili = Convert.ToDouble(sr.ReadToEnd());
+                }
+                if (fInfo.AlternateDataStreamExists(StreamNameReviewedTimes))
+                {
+                    StreamReader sr = fInfo.GetAlternateDataStream(StreamNameReviewedTimes).OpenText();
+                    review = Convert.ToInt64(sr.ReadToEnd());
+                }
+
+                words.Add(new Word(curName, curMeaning, famili, review));
+            }
+        }
+
+        public static void WriteWordInfo(Word newWord)
+        {
+            FileInfo fInfo = new FileInfo(WorkingDir + newWord.WordName);
+            if (fInfo.AlternateDataStreamExists(StreamNameFamili))
+                fInfo.DeleteAlternateDataStream(StreamNameFamili);
+            if (fInfo.AlternateDataStreamExists(StreamNameReviewedTimes))
+                fInfo.DeleteAlternateDataStream(StreamNameReviewedTimes);
+
+            FileStream fsFamili = fInfo.GetAlternateDataStream(StreamNameFamili, FileMode.Create).OpenWrite();
+            byte[] bytesFamili = Encoding.ASCII.GetBytes(newWord.Familiarity.ToString());
+            fsFamili.Write(bytesFamili, 0, bytesFamili.Length);
+            fsFamili.Close();
+
+            FileStream fsReview = fInfo.GetAlternateDataStream(StreamNameReviewedTimes, FileMode.Create).OpenWrite();
+            byte[] bytesReview = Encoding.ASCII.GetBytes(newWord.ReviewedTimes.ToString());
+            fsReview.Write(bytesReview, 0, bytesReview.Length);
+            fsReview.Close();
+
+
+        }
+
+        private const string WorkingDir = @"E:\Documents\Words\";
+        private const string StreamNameFamili = @"Familiarity";
+        private const string StreamNameReviewedTimes = @"ReviewedTimes";
+        //public static void Loa
+    }
+
+}
---
+++ 单词本/单词本/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace 单词本
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonReload_Click(object sender, RoutedEventArgs e)
+        {
+            ClassIO.LoadWords();
+            curWords = ClassIO.words;
+        }
+
+        private List<Word> curWords = new List<Word>();
+        private Word curWord = null;
+        private int curPlace = 0;
+
+        private void buttonNext_Click(object sender, RoutedEventArgs e)
+        {
+            if (curWords == null)
+            {
+                MessageBox.Show("Not loaded words");
+            }
+            else
+            {
+                Random ran = new Random();
+                curPlace = ran.Next(curWords.Count);
+                loadWord(curWords[curPlace]);
+            }
+        }
+
+        private void loadWord(Word curword)
+        {
+            curWord = curword;
+            textBoxWord.Text = curword.WordName;
+            textBlockWORD.Text = curword.WordName.ToUpper();
+            //textBlockMeaning.Text = curword.Meaning;
+        }
+
+        private void textBoxMeaning_MouseLeave(object sender, MouseEventArgs e)
+        {
+            textBoxMeaning.Text = string.Empty;
+        }
+
+        private void textBoxMeaning_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (curWord == null)
+                return;
+            textBoxMeaning.Text = curWord.Meaning;
+        }
+
+        private void buttonLevels_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn != null)
+            {
+                if ((btn.Content as string).Contains('0'))
+                {
+                    updateTheWrod(0);
+                    //return;
+                }
+                if ((btn.Content as string).Contains('1'))
+                {
+                    updateTheWrod(1);
+                    //return;
+                }
+                if ((btn.Content as string).Contains('2'))
+                {
+                    updateTheWrod(2);
+                    //return;
+                }
+                if ((btn.Content as string).Contains('3'))
+                {
+                    updateTheWrod(3);
+                    //return;
+                }
+                if ((btn.Content as string).Contains('4'))
+                {
+                    updateTheWrod(4);
+                    //return;
+                }
+                buttonNext_Click(null, null);
+            }
+        }
+
+        private void updateTheWrod(int level)
+        {
+            double newFamili = 0;
+            long newReview = 1;
+
+            newFamili = curWords[curPlace].Familiarity;
+            newReview = curWords[curPlace].ReviewedTimes;
+
+            double sum = newFamili * newReview + level;
+            newReview++;
+            newFamili = sum / newReview;
+
+            curWords[curPlace].Familiarity = newFamili;
+            curWords[curPlace].ReviewedTimes = newReview;
+            ClassIO.words[curPlace].Familiarity = newFamili;
+            ClassIO.words[curPlace].ReviewedTimes = newReview;
+
+            ClassIO.WriteWordInfo(curWords[curPlace]);
+        }
+
+    }
+}
