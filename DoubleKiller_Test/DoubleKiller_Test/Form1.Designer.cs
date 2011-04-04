83643608cf601eaac7da986cf09f2c082167b3cf
DOSSTONED <dosstoned@vista.aero>
2011-04-04 21:05:10 +0800

    Init commit for project(auto) DoubleKiller_Test

---
+++ DoubleKiller_Test/DoubleKiller_Test/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace DoubleKiller_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonScan = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(12, 12);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 0;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // buttonScan
+            // 
+            this.buttonScan.Location = new System.Drawing.Point(93, 12);
+            this.buttonScan.Name = "buttonScan";
+            this.buttonScan.Size = new System.Drawing.Size(75, 23);
+            this.buttonScan.TabIndex = 1;
+            this.buttonScan.Text = "Scan";
+            this.buttonScan.UseVisualStyleBackColor = true;
+            this.buttonScan.Click += new System.EventHandler(this.buttonScan_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(405, 342);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(174, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Scan Empty Dir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(279, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Delete Empty Dir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 396);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonScan);
+            this.Controls.Add(this.buttonLoad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonScan;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DoubleKiller_Test/DoubleKiller_Test/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DoubleKiller_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string[] filesPaths = Directory.GetFiles(@"D:\Program\test", "*.*", SearchOption.AllDirectories);
+            FileInfo[] filesInfos = new FileInfo[filesPaths.Length];
+            listBox1.Items.AddRange(filesPaths);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string[] dirs = Directory.GetDirectories(@"D:\Program\test", "*", SearchOption.AllDirectories);
+
+            listBox1.Items.AddRange(dirs);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] dirInfos = new string[listBox1.Items.Count];
+            for (int i = 0; i < dirInfos.Length; i++)
+            {
+                dirInfos[i] = listBox1.Items[i] as string;
+                if (dirInfos[i] != null)
+                {
+                    if (Directory.GetDirectories(dirInfos[i]) == null && Directory.GetFiles(dirInfos[i]) == null)
+                        Directory.Delete(dirInfos[i]);
+                }
+            }
+            
+        }
+
+        private void buttonScan_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string[] fileInfos = new string[listBox1.Items.Count];
+            for (int i = 0; i < fileInfos.Length; i++)
+            {
+                fileInfos[i] = listBox1.Items[i] as string;
+                if (fileInfos[i] != null)
+                {
+                }
+            }
+
+
+            // Create a delicious data source.
+            string[] fruits = { "cherry", "apple", "blueberry" };
+
+            // Query for ascending sort.
+            IEnumerable<string> sortAscendingQuery =
+                from fruit in fruits
+                orderby fruit //"ascending" is default
+                select fruit;
+
+            // Query for descending sort.
+            IEnumerable<string> sortDescendingQuery =
+                from w in fruits
+                orderby w.Length descending
+                select w;
+
+            // Execute the query.
+            Console.WriteLine("Ascending:");
+            foreach (string s in sortAscendingQuery)
+            {
+                listBox1.Items.Add(s);//Console.WriteLine(s);
+            }
+
+            // Execute the query.
+            Console.WriteLine(Environment.NewLine + "Descending:");
+            foreach (string s in sortDescendingQuery)
+            {
+                listBox1.Items.Add(s);//Console.WriteLine(s);
+            }
+
+        }
+    }
+}
