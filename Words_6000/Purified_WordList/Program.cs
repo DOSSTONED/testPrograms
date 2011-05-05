1ebb4abc76b956da6896478d0a38d0cdd7461700
DC-Shi <daochen.shi@gmail.com>
2011-05-05 20:37:21 +0800

    Init commit for project(auto) Words_6000

---
+++ Words_6000/Purified_WordList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Purified_WordList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wordLines = File.ReadAllLines(@"E:\Documents\GRE.htm");
+            string[] words = new string[wordLines.Length];
+            int totalLineNumber = 0;
+
+            for (int i = 0; i < wordLines.Length; i++)
+            {
+                if (wordLines[i].Contains("3g.dict.cn"))
+                {
+                    words[totalLineNumber] = wordLines[i].Substring(wordLines[i].IndexOf("s.php?q=") + 8);
+                    words[totalLineNumber] = words[totalLineNumber].Substring(0, words[totalLineNumber].IndexOf('\"'));
+                    if (words[totalLineNumber].Contains("20"))
+                    {
+                        continue;
+                    }
+                    totalLineNumber++;
+                    continue;
+                }
+                //wordLines[i] = string.Empty;
+            }
+            string[] writtenWords = new string[totalLineNumber];
+            for (int i = 0; i < totalLineNumber; i++)
+            {
+                writtenWords[i] = words[i];
+            }
+            
+            File.WriteAllLines(@"E:\Documents\GRE.txt", writtenWords);
+        }
+    }
+}
---
+++ Words_6000/Slider_Test/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Slider_Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+            Point p = e.GetPosition(this);
+            label1.Content = p.ToString();
+
+            double curHorPos = p.X;
+
+            //setButtonSize(largeSize - normalSize, button1);
+            //setButtonALLSize(curHorPos, 650);
+
+        }
+
+        void setButtonALLSize(double curHorPos, double total)
+        {
+            int normalSize = 64;
+            int largeSize = 128;
+            setButtonSize(normalSize + scale(10 * total / 650, 160 * total / 650, curHorPos) * (largeSize - normalSize), ref button1);
+            setButtonSize(normalSize + scale(130 * total / 650, 280 * total / 650, curHorPos) * (largeSize - normalSize), ref button2);
+            setButtonSize(normalSize + scale(250 * total / 650, 400 * total / 650, curHorPos) * (largeSize - normalSize), ref button3);
+            setButtonSize(normalSize + scale(370 * total / 650, 520 * total / 650, curHorPos) * (largeSize - normalSize), ref button4);
+            setButtonSize(normalSize + scale(490 * total / 650, 640 * total / 650, curHorPos) * (largeSize - normalSize), ref button5);
+        }
+
+        double scale(double start, double end, double curPlace)   // 
+        {
+            if (curPlace < start)
+                return 0;
+            if (curPlace > end)
+                return 0;
+            if (curPlace < start + (end - start) / 3)
+                return (curPlace - start) * 3 / (end - start);
+            if (curPlace > start + (end - start) * 2 / 3)
+                return (1 - (curPlace - start) / (end - start)) * 3;
+            return 1;
+        }
+
+        void setButtonSize(double size, ref Button btn)
+        {
+            double center = btn.Width / 2 + btn.Margin.Left;
+
+            
+            btn.Width = size;
+            btn.Height = size;
+            //btn.Margin.Left = center - size / 2;
+            btn.Margin = new Thickness(center - size / 2, 0, 0, 0);
+        }
+
+        private SlideBar sb = new SlideBar(true);
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            sb.sbArriveEvent += new SlideBarEventHandler(sb_sbArriveEvent);
+        }
+
+        void sb_sbArriveEvent(SlideBar.SlideBarData sbData)
+        {
+            MessageBox.Show("");
+            setButtonALLSize(sbData.bPosition, 256);//throw new NotImplementedException();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            sb.sbArriveEvent -= new SlideBarEventHandler(sb_sbArriveEvent);
+            sb = null;
+        }
+    }
+}
