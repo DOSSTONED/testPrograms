6cdce829515d9f0bda2ac1ff2d1e38374e0e87e9
DOSSTONED <dosstoned@vista.aero>
2011-09-17 22:51:01 +0800

    Init commit for project TMP_WindowsFormsApplication1

---
+++ TMP_WindowsFormsApplication1/TMP_WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TMP_WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    for (int k = 0; k < 3; k++)
+                        for (int l = 0; l < 2; l++)
+                            textBox1.Text += i.ToString() + j.ToString() + k.ToString() + l.ToString() + "\r\n";
+        }
+    }
+}
