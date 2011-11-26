d816ca6dcff93b934d74e5725ae93982b94a1346
DOSSTONED <DOSSTONED@vista.aero>
2011-11-26 23:26:37 +0800

    Auto init commit for project Cipher

---
+++ Cipher/位移加密/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 位移加密
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            for (int k = 0; k < 26; k++)
+                for (int i = 0; i < 26; i++)
+                {
+                    //i(105)->o(111), x(120)->c(99)
+                    if ((((105 - 97) * i + k) % 26 == 111 % 26) && (((120 - 97) * i + k) % 26 == 99 % 26))
+                    {
+                        for (int j = 0; j < input.Length; j++)
+                            Console.Write((char)(97 + ((input[j] - 97) * i + k) % 26));
+                        Console.WriteLine();
+                    }
+                }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Cipher/文件转位图/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace 文件转位图
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random ran = new Random();
+            //byte[] imageData = Encoding.Default.GetBytes(textBox1.Text);
+            byte[] imageData = new byte[pictureBox1.Width * pictureBox1.Height * 4];
+            for (int i = 0; i < imageData.Length; i++)
+                imageData[i] = (byte)ran.Next(0, 255);
+            unsafe
+            {
+                fixed (byte* ptr = imageData)
+                {
+                    using (Bitmap image = new Bitmap(pictureBox1.Width, pictureBox1.Height, pictureBox1.Width * 4, PixelFormat.Format32bppArgb, new IntPtr(ptr)))
+                    {
+                        pictureBox1.Image = image;
+                        //image.Save(fileName);
+                    }
+                }
+            }
+        }
+    }
+}
