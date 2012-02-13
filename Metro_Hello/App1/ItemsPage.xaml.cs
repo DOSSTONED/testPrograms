f64afd98dee6729daa1c84e29cd51795fc35be62
DC-Shi <daochen.shi@gmail.com>
2012-02-14 04:21:16 +0800

    Init commit for project Metro_Hello

---
+++ Metro_Hello/App1/ItemsPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using App1.Data;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “项目页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234233 上提供
+
+namespace App1
+{
+    /// <summary>
+    /// 显示项预览集合的页。在“拆分应用程序”中，此页
+    /// 用于显示及选择可用组之一。
+    /// </summary>
+    public sealed partial class ItemsPage : App1.Common.LayoutAwarePage
+    {
+        public ItemsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+            // TODO: Create an appropriate data model for your problem domain to replace the sample data
+            var sampleDataGroups = SampleDataSource.GetGroups((String)navigationParameter);
+            this.DefaultViewModel["Items"] = sampleDataGroups;
+        }
+
+        /// <summary>
+        /// 在单击某个项时进行调用。
+        /// </summary>
+        /// <param name="sender">显示所单击项的 GridView (在应用程序处于对齐状态时
+        /// 为 ListView)。</param>
+        /// <param name="e">描述所单击项的事件数据。</param>
+        void ItemView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // 导航至相应的目标页，并
+            // 通过将所需信息作为导航参数传入来配置新页
+            var groupId = ((SampleDataGroup)e.ClickedItem).UniqueId;
+            this.Frame.Navigate(typeof(SplitPage), groupId);
+        }
+    }
+}
