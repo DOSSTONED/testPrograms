14232c77122b2391014a5664ad5174e4e756ff62
DOSSTONED <dosstoned@vista.aero>
2012-04-24 01:13:17 +0800

    Init commit for project(auto) libUSB_Test

---
+++ libUSB_Test/libUSB_Test/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace libUSB_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.lblConnState = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(111, 38);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 0;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(111, 9);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(5, 11);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(100, 21);
+            this.txtSend.TabIndex = 2;
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(12, 175);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(23, 12);
+            this.lblMsg.TabIndex = 3;
+            this.lblMsg.Text = "msg";
+            // 
+            // lblConnState
+            // 
+            this.lblConnState.AutoSize = true;
+            this.lblConnState.Location = new System.Drawing.Point(12, 118);
+            this.lblConnState.Name = "lblConnState";
+            this.lblConnState.Size = new System.Drawing.Size(77, 12);
+            this.lblConnState.TabIndex = 4;
+            this.lblConnState.Text = "lblConnState";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblConnState);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.Label lblConnState;
+    }
+}
+
