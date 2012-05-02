3d7c95bdfd15ed86264813fe292f502f1aec6031
DOSSTONED <DOSSTONED@vista.aero>
2012-05-02 23:59:51 +0800

    init project Begin with 4(DataBound,DialOnly,Hello,IPv6_Test)

---
+++ Begin/DialOnly/Class/ChooserListener.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Phone.Shell.Interop;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace DialOnly.Class
+{
+    public static class ChooserListener
+    {
+        //private static IChooser _warmChooser;
+        //private static ChooserInfo _pendingChooserInfo;
+        //private static bool _fPendingChooserDeserialized;
+        //private static InvokeReturningEventArgs _pendingChooserArgs;
+        //private static bool _isInitialized;
+        //internal static bool IsChooserPending
+        //{
+        //    get
+        //    {
+        //        return ChooserListener._warmChooser != null;
+        //    }
+        //}
+        //static ChooserListener()
+        //{
+        //    ChooserListener.Initialize();
+        //}
+        //public static void Initialize()
+        //{
+        //    if (!ChooserListener._isInitialized)
+        //    {
+        //        ShellPageManager instance = ShellPageManager.Instance;
+        //        if (instance != null)
+        //        {
+        //            ShellPageManagerCallback expr_16 = instance.ShellPageManagerCallback;
+        //            expr_16.OnInvokeReturningEventHandler = (EventHandler<InvokeReturningEventArgs>)Delegate.Combine(expr_16.OnInvokeReturningEventHandler, new EventHandler<InvokeReturningEventArgs>(ChooserListener.OnInvokeReturning));
+        //        }
+        //        ChooserListener._isInitialized = true;
+        //    }
+        //}
+        //internal static void RegisterChooser(IChooser chooser)
+        //{
+        //    if (ChooserListener._warmChooser != null)
+        //    {
+        //        throw new InvalidOperationException("Not allowed to call Show() multiple times before an invocation returns");
+        //    }
+        //    ChooserListener._warmChooser = chooser;
+        //    ChooserListener._pendingChooserInfo = null;
+        //    ChooserListener._pendingChooserArgs = null;
+        //    ChooserListener._fPendingChooserDeserialized = false;
+        //}
+        //internal static void DeregisterChooser()
+        //{
+        //    ChooserListener._warmChooser = null;
+        //}
+        //internal static void FireChooserComplete(InvokeReturningEventArgs chooserArgs, IChooser chooser)
+        //{
+        //    if (chooserArgs.stateBuffer != null)
+        //    {
+        //        chooser.Deserialize(new MemoryStream(chooserArgs.stateBuffer));
+        //    }
+        //    chooser.OnInvokeReturned(chooserArgs.returnBuffer, null);
+        //}
+        //internal static bool IsChooserCompletePending(IChooser chooser, Delegate handler, out InvokeReturningEventArgs args)
+        //{
+        //    bool result = false;
+        //    args = null;
+        //    if (ChooserListener._pendingChooserInfo != null && string.Equals(ChooserInfo.ChooserTypeFromChooser(chooser), ChooserListener._pendingChooserInfo.ChooserType) && ChooserListener._pendingChooserInfo.Observers != null)
+        //    {
+        //        string key = ChooserInfo.ObserverFromDelegate(handler);
+        //        if (ChooserListener._pendingChooserInfo.Observers.ContainsKey(key))
+        //        {
+        //            if (!ChooserListener._fPendingChooserDeserialized)
+        //            {
+        //                if (ChooserListener._pendingChooserArgs.stateBuffer != null)
+        //                {
+        //                    chooser.Deserialize(new MemoryStream(ChooserListener._pendingChooserArgs.stateBuffer));
+        //                }
+        //                ChooserListener._fPendingChooserDeserialized = true;
+        //            }
+        //            ChooserListener._pendingChooserInfo.Observers.Remove(key);
+        //            args = ChooserListener._pendingChooserArgs;
+        //            result = true;
+        //        }
+        //    }
+        //    return result;
+        //}
+        //private static void OnInvokeReturning(object sender, InvokeReturningEventArgs args)
+        //{
+        //    if (ChooserListener._warmChooser != null)
+        //    {
+        //        ChooserListener.FireChooserComplete(args, ChooserListener._warmChooser);
+        //        return;
+        //    }
+        //    MemoryStream stream = new MemoryStream(args.correlationBlob);
+        //    DataContractSerializer dataContractSerializer = new DataContractSerializer(typeof(ChooserInfo));
+        //    ChooserListener._pendingChooserInfo = (ChooserInfo)dataContractSerializer.ReadObject(stream);
+        //    ChooserListener._pendingChooserArgs = args;
+        //}
+    }
+}
---
+++ Begin/DialOnly/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Phone.Controls;
+using DialOnly.Class;
+//using Microsoft.Phone.Tasks;
+
+namespace DialOnly
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonNum_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            if (b == null) return;
+            PageTitle.Text += b.Content as string;
+        }
+
+        private void buttonClear_Click(object sender, RoutedEventArgs e)
+        {
+            if (PageTitle.Text.Length < 1) return;
+            PageTitle.Text = PageTitle.Text.Substring(0, PageTitle.Text.Length - 1);
+        }
+
+        private void buttonCall_Click(object sender, RoutedEventArgs e)
+        {
+            PhoneCallTask pct = new PhoneCallTask();
+            //pct.DisplayName = "Test";
+            pct.PhoneNumber = PageTitle.Text;
+            pct.Show();
+            
+        }
+
+        private void buttonMe_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Thanks for use this dialer, made by DOSSTONED", "Hi~", MessageBoxButton.OK);
+        }
+    }
+}
