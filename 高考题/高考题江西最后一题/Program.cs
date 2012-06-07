d910159996e2b1f13e6bd028dbb4d21022265939
DC-Shi <daochen.shi@gmail.com>
2012-06-07 19:02:59 +0800

    Init commit for project(auto) 高考题

---
+++ 高考题/高考题江西最后一题/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 高考题江西最后一题
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double an = 0, an_1 = 0;
+            double c = 0.2501;
+            int loop = 0;
+            while (loop<50)
+            {
+                loop++;
+                an = -an_1 * an_1 + an_1 + c;
+                //if (an < an_1)
+                //{
+                Console.WriteLine("loop={0},{1}", loop, an);
+                //    break;
+                //}
+                an_1 = an;
+            }
+        }
+    }
+}
