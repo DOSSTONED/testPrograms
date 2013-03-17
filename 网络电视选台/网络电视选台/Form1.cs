16a6f5abf37808d02b93a21b39a1f6bff03259b4
DC-Shi <daochen.shi@gmail.com>
2013-03-17 22:19:15 +0800

    Init commit for project 网络电视选台

---
+++ 网络电视选台/网络电视选台/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+//using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vlc.DotNet.Forms;
+
+namespace 网络电视选台
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VlcControl vc = new VlcControl();
+            Vlc.DotNet.Core.Medias.PathMedia lm = new Vlc.DotNet.Core.Medias.PathMedia(@"[探索频道-重返51区].Discovery.Channel.Return.To.Area.51.PDTV.XviD-HEH.avi");
+            vc.Play(lm);
+            
+        }
+    }
+}
---
+++ 网络电视选台/网络电视选台/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vlc.DotNet.Core;
+
+namespace 网络电视选台
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            //Set libvlc.dll and libvlccore.dll directory path
+            //VlcContext.LibVlcDllsPath = CommonStrings.LIBVLC_DLLS_PATH_DEFAULT_VALUE_X86;
+            //Set the vlc plugins directory path
+            //VlcContext.LibVlcPluginsPath = CommonStrings.PLUGINS_PATH_DEFAULT_VALUE_X86;
+            VlcContext.LibVlcDllsPath = @"VLCPortable\App\vlc\";
+            VlcContext.LibVlcPluginsPath = @"VLCPortable\App\vlc\plugins\";
+
+            //Set the startup options
+            VlcContext.StartupOptions.IgnoreConfig = true;
+            VlcContext.StartupOptions.LogOptions.LogInFile = true;
+            VlcContext.StartupOptions.LogOptions.ShowLoggerConsole = true;
+            VlcContext.StartupOptions.LogOptions.Verbosity = VlcLogVerbosities.Debug;
+            
+
+            //Initialize the VlcContext
+            VlcContext.Initialize(); 
+            Application.Run(new Form1());
+
+
+            //Close the VlcContext
+            VlcContext.CloseAll(); 
+        }
+    }
+}
