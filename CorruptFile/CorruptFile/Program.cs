455bad54e54ebbcad9ca964626133ea854a33d69
DOSSTONED <DOSSTONED@vista.aero>
2015-08-19 10:11:36 +0800

    Auto init commit for project CorruptFile

---
+++ CorruptFile/CorruptFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CorruptFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"T:\Downloads\Music\世界十大背景音乐无损\He's A Pirate - Klaus Badelt.flac";
+            var bytes = File.ReadAllBytes(filePath);
+            bytes[bytes.Length - 1] = 0;
+            File.WriteAllBytes(filePath, bytes);
+        }
+    }
+}
